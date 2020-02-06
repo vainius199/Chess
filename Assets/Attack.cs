c3f76a0a58db12a731f634c376ea202784135160
Vainius <vainius199@gmail.com>
2020-02-06 15:04:19 +0200

    first commit

---
+++ Assets/Attack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+   public Death death;
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        death.DeathAnim();
+        Debug.Log("Stuff");
+        anim.Play("knight_attackSpecial");
+    }
+
+    
+}
---
+++ Assets/Death.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+
+    private Animator anim;
+    
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void DeathAnim()
+    {
+       
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.5f);
+        
+        StartCoroutine("Wait");
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1.5f);
+        anim.Play("knight_die");
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+        
+    }
+
+}
